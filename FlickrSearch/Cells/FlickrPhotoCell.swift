536faf6916d93ee213fbedc02b4ea54f67a5f567
nikitanesporov <nikitanesporov@gmail.com>
2022-08-03 23:50:07 +0500

    Add Flickr Photo Cell

---
+++ FlickrSearch/Cells/FlickrPhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  FlickrPhotoCell.swift
+//  FlickrSearch
+//
+//  Created by Nikita Nesporov on 03.08.2022.
+//
+
+import UIKit
+
+class FlickrPhotoCell: UICollectionViewCell {
+    
+    
+    
+}
